0bb15d2fe2a4c04c9d193fbf24e510ded6d6ca08
Arseniy Chernov <arschernov@yandex.ru>
2019-03-02 23:55:53 +0500

    appears to be working with margins and paddings

--- HW3ViewGroup/app/src/main/java/com/example/arseniy/hw3_viewgroup/MyViewGroup.java
+++ HW3ViewGroup/app/src/main/java/com/example/arseniy/hw3_viewgroup/MyViewGroup.java
@@ -2,17 +2,14 @@ package com.example.arseniy.hw3_viewgroup;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.text.Layout;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MyViewGroup extends FrameLayout {
     int mHorizontalSpace;
     int mVerticalSpace;
+    int mRowHeight;
 
     public MyViewGroup(Context context) {
         super(context);
@@ -32,29 +29,32 @@ public class MyViewGroup extends FrameLayout {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyViewGroup);
         mHorizontalSpace = typedArray.getDimensionPixelSize(R.styleable.MyViewGroup_mvg_horizontal_space_size, 0);
         mVerticalSpace = typedArray.getDimensionPixelSize(R.styleable.MyViewGroup_mvg_vertical_space_size, 0);
+        mRowHeight = typedArray.getDimensionPixelSize(R.styleable.MyViewGroup_mvg_height, -1);
         typedArray.recycle();
     }
-
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int count = getChildCount();
 
-        int maxHeight = 0;
-        int maxWidth = 0;
+        // Максимальное накопленное значение высоты
+        int sumHeight = this.getPaddingTop() + this.getPaddingBottom();
 
-        int specSizeWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int specModeWidth = MeasureSpec.getMode(widthMeasureSpec);
-        int specSizeHeight = MeasureSpec.getSize(heightMeasureSpec);
-        int specModeHeight = MeasureSpec.getMode(heightMeasureSpec);
+        int rowWidth = this.getPaddingLeft() + this.getPaddingRight();
+        int rowHeight = 0;
+
+        int specWidthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
 
         int childWidth;
         int childHeight;
+        // флаг, переключающий размер строчки между фиксированным и динамическим (высота = максимально высокий элемент в строчке)
+        boolean fixedHeight = (mRowHeight != -1);
+        // флаг, определяющий сделали ли мы перенос на следующую строку
+        boolean reachedMaxWidth = false;
 
         measureChildren(widthMeasureSpec, heightMeasureSpec);
 
-
-        // Find rightmost and bottom-most child
         for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
             LayoutParams childLP = (LayoutParams) child.getLayoutParams();
@@ -62,29 +62,29 @@ public class MyViewGroup extends FrameLayout {
             if (child.getVisibility() != GONE) {
                 childWidth = childLP.leftMargin + child.getMeasuredWidth() + childLP.rightMargin;
                 childHeight = childLP.topMargin + child.getMeasuredHeight() + childLP.bottomMargin;
+                rowHeight = fixedHeight ? mRowHeight : Math.max(rowHeight, childHeight);
 
-                if ((maxWidth + childWidth) > specSizeWidth) {
-                    maxHeight += childHeight;
+                //если накопленная ширина + текущая больше дозволенных рамок, переносим строку
+                if ((rowWidth + childWidth) > specWidthSize && widthMode != MeasureSpec.UNSPECIFIED) {
+                    reachedMaxWidth = true;
+                    sumHeight += rowHeight + mVerticalSpace;
+                    if (!fixedHeight)
+                        // сбрасываем высоту новой строчки на высоту элемента
+                        rowHeight = childHeight;
+                    rowWidth = childWidth + mHorizontalSpace + this.getPaddingLeft() + this.getPaddingRight();
                 }
                 else {
-                    maxWidth += childWidth + mHorizontalSpace;
-                    maxHeight = Math.max(maxHeight, childHeight);
+                    rowWidth += childWidth + mHorizontalSpace;
                 }
             }
         }
+        sumHeight += rowHeight;
 
-        // Account for padding too
-        maxWidth +=  this.getPaddingLeft() + this.getPaddingRight();
-        maxHeight += this.getPaddingTop() + this.getPaddingBottom();
-
-        // Check against minimum height and width
-        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
-        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
-
-
-        int resolved1 = resolveSize(maxWidth, widthMeasureSpec);
-        int resolved2 = resolveSize(maxHeight, heightMeasureSpec);
-        setMeasuredDimension(resolved1, resolved2);
+        if (reachedMaxWidth || widthMode == MeasureSpec.EXACTLY)
+            setMeasuredDimension(specWidthSize, resolveSize(sumHeight, heightMeasureSpec));
+        else if (widthMode == MeasureSpec.AT_MOST || widthMode == MeasureSpec.UNSPECIFIED) {
+            setMeasuredDimension(rowWidth, resolveSize(sumHeight, heightMeasureSpec));
+        }
     }
 
     @Override
@@ -95,27 +95,30 @@ public class MyViewGroup extends FrameLayout {
 
         int childWidth;
         int childHeight;
+        l += this.getPaddingLeft();
+        r -= this.getPaddingRight();
+        int width = r - l;
 
         int rowHeight = 0;
+        boolean fixedHeight = (mRowHeight != -1);
 
         for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
             LayoutParams childLP = (LayoutParams) child.getLayoutParams();
 
             if (child.getVisibility() != GONE) {
-                int lm = childLP.leftMargin;
-                childWidth = lm + child.getMeasuredWidth() + childLP.rightMargin;
-                childHeight = childLP.topMargin + child.getMeasuredHeight() + childLP.bottomMargin ;
+                childWidth = childLP.leftMargin + child.getMeasuredWidth() + childLP.rightMargin;
+                childHeight = fixedHeight ? mRowHeight : childLP.topMargin + child.getMeasuredHeight() + childLP.bottomMargin ;
                 rowHeight = Math.max(rowHeight, childHeight);
 
-                if ((widthOffset + childWidth) > r) {
+                if ((widthOffset + childWidth) > width) {
                     heightOffset += rowHeight + mVerticalSpace;
                     widthOffset = 0;
                 }
-                child.layout(widthOffset + childLP.leftMargin,
-                        heightOffset + childLP.topMargin,
-                        widthOffset + childWidth - childLP.rightMargin,
-                        heightOffset + childHeight - childLP.topMargin);
+                child.layout( this.getPaddingLeft() + widthOffset + childLP.leftMargin,
+                        this.getPaddingTop() + heightOffset + childLP.topMargin,
+                        this.getPaddingLeft() + widthOffset + childWidth - childLP.rightMargin,
+                        this.getPaddingTop() + heightOffset + childHeight - childLP.bottomMargin);
                 widthOffset += childWidth + mHorizontalSpace;
             }
             else {
